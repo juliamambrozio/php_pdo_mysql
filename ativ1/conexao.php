a4c8785904ca52312cce70784cd4e9d4faa5ba26
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-11-23 17:30:52 -0300

    tratando erros

--- ativ1/conexao.php
+++ ativ1/conexao.php
@@ -2,11 +2,20 @@
 
 //CONEXÃO DE COMUNICAÇÃO COM O BD
 
-$dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+$dsn = 'mysql:host=localhost;dbname=php_com_pdo'; // //DSN (nome da fonte de dados = mysql) nome do bd
 $usuario = 'root';
 $senha = 'julia9122';
 
-$conexao = new PDO($dsn, $usuario,  $senha); //DSN (nome da fonte de dados = mysql)
+
+
+try{
+    $conexao = new PDO($dsn, $usuario,  $senha);
+
+}catch(PDOException $e){
+    echo 'Erro: '. $e->getCode().' Mensagem: '. $e->getMessage(); //código do erro, onde está o erro
+    //registrar erro
+
+}
 
 
 ?>
